6ea06794712ac36c7585f27ae0a36f3a4fd76a20
HenryVooren <henry.vooren@my.jcu.edu.au>
2021-08-27 15:02:29 +1000

    Initial commit

---
+++ Prac_04/lists_warmup.py
@@ -0,0 +1,37 @@
+numbers = [3, 1, 4, 1, 5, 9, 2]
+
+"""
+Questions :
+
+1. numbers[0] = 3
+2. numbers[-1] = 2
+3. numbers[3] = 1
+4. numbers[:-1] = [3, 1, 4, 1, 5, 9, 2]
+5. numbers[3:4] = [1]
+6. 5 in numbers = True
+7. 7 in numbers = False
+8. "3" in numbers = False
+9. numbers + [6, 5, 3] = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3]
+
+"""
+
+# Tasks
+
+# 1. Change the first element of numbers to "ten" (the string, not the number 10)
+
+numbers[0] = 10
+print(numbers)
+
+# 2. Change the last element of numbers to 1
+
+numbers[-1] = 1
+print(numbers)
+
+# 3. Get all the elements from numbers except the first two (slice)
+
+numbers_slice = numbers[2:]
+print(numbers_slice)
+
+# 4. Check if 9 is an element of numbers
+
+print(9 in numbers)
---
+++ Prac_04/total_income.py
@@ -0,0 +1,28 @@
+"""
+CP1404/CP5632 Practical
+Starter code for cumulative total income program
+"""
+
+
+def report_income(num_months, incomes):
+    print("\nIncome Report\n-------------")
+    total = 0
+    for month in range(1, num_months + 1):
+        income = incomes[month - 1]
+        total += income
+        print("Month {:2} - Income: ${:10.2f} Total: ${:10.2f}".format(month, income, total))
+
+
+def main():
+    """Display income report for incomes over a given number of months."""
+    incomes = []
+    num_months = int(input("How many months? "))
+
+    for month in range(1, num_months + 1):
+        income = float(input("Enter income for month {:2.0f}: ".format(month)))
+        incomes.append(income)
+
+    report_income(num_months, incomes)
+
+
+main()
